5195f838ee3e94296ac25333a23a7d7d663123a8
Nathan Peterson <89316572+swipedsalad@users.noreply.github.com>
2022-11-12 23:58:00 -0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,8 +7,5 @@ from adafruit_ads1x15.analog_in import AnalogIn
 ads = ADS.ADS1015(i2c)
 chan = AnalogIn(ads, ADS.P0)
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)  example with multiple pins being used
-print(chan.value, chan.voltage)
-
-# Test code
-# temp = (chan.voltage - 0.5) * 100
-# print(temp)
+temp = chan.voltage * 100
+print(temp)
