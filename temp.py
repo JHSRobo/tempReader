8d88690edb6ae1bfbbbf05e2844d0ff889aed22d
Nathan Peterson <89316572+swipedsalad@users.noreply.github.com>
2022-11-12 23:29:29 -0800

    Update temp.py
    Made some code that needs testing on the test bench

--- temp.py
+++ temp.py
@@ -1,1 +1,14 @@
+import board
+import busio
+i2c = busio.I2C(board.SCL, board.SDA)
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
+ads = ADS.ADS1015(i2c)
+chan = AnalogIn(ads, ADS.P0)
+# chan = AnalogIn(ads, ADS.P0, ADS.P1)  example with multiple pins being used
+print(chan.value, chan.voltage)
+
+# Test code
+# temp = (chan.voltage - 0.5) * 100
+# print(temp)
